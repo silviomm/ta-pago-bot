f475ab81dd80982c25c090da60d3d710624af2be
silviomm <silvio.mattos@vtex.com>
2020-06-27 23:45:11 -0300

    adding mongo setup

---
+++ src/db/mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+var url = process.env.MONGO_URL || "mongodb://localhost:27017/tapago";
+var dbConfig = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // user: 'root',
+    // pass: 'root'
+}
+
+mongoose.connect(url, dbConfig).then(() => {
+    console.log('Connected to tapago db!');
+}).catch(err => {
+    console.log('error connecting to the database');
+    process.exit();
+});
