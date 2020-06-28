c91cb49833a6d687d956233a7bcd440479e85c18
silviomm <silvio.mattos@vtex.com>
2020-06-28 11:25:14 -0300

    update env for mongodb

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-var url = process.env.MONGO_URL || "mongodb://localhost:27017/tapago";
+var url = process.env.MONGODB_URI || "mongodb://localhost:27017/tapago";
 var dbConfig = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
